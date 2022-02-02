da39c991926b49f31a46ff8a670e84a1547cf7ab
Claudio <claudiodicaprio89@gmail.com>
2022-02-02 17:59:16 +0100

    it's a kind of magic

--- resources/views/partials/main.blade.php
+++ resources/views/partials/main.blade.php
@@ -1,3 +1,14 @@
 <main class="main">
+    <span class="current-series">CURRENT SERIES</span>
 
+   <div class="conta">
+        @foreach($comics as $comic)   
+            <div class="comic-sachet">
+                <a href="">
+                    <img class="comic-cover" src="{{$comic['thumb']}}" alt="{{$comic['title']}}">
+                    <span class="comic-title">{{$comic['title']}}</span>
+                </a>
+            </div>
+        @endforeach
+   </div>
 </main>
