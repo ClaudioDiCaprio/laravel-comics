e570fb3e8efeaf13f59c37d953641d00954f21d8
Claudio <claudiodicaprio89@gmail.com>
2022-02-02 16:27:26 +0100

    first commit

---
+++ resources/views/home.blade.php
@@ -0,0 +1,9 @@
+@extends('layouts.base')
+
+@section('pageTitle')
+home
+@endsection
+
+@section('pageContent')
+<h1>home page</h1>
+@endsection
---
+++ resources/views/partials/header.blade.php
@@ -0,0 +1,44 @@
+<header>
+   <div class="container">
+    <a href="">
+        <img src="{{asset('img/dc-logo.png')}}" alt="DC_Logo">
+    </a>
+    <nav class="main-nav">
+        <ul>
+            <li >
+                <a href="/characters">Characters</a>
+            </li>
+            <li >
+                <a href="/comics">comics</a>
+            </li>
+            <li >
+                <a href="/movies">movies</a>
+            </li>
+            <li >
+                <a href="/tv">tv</a>
+            </li>
+            <li >
+                <a href="/games">games</a>
+            </li>
+            <li >
+                <a href="/collectibles">collectibles</a>
+            </li>
+            <li >
+                <a href="/videos">videos</a>
+            </li>
+            <li>
+                <a href="/fans">fans</a>
+            </li>
+            <li >
+                <a href="/news">news</a>
+            </li>
+            <li >
+                <a href="/shop">shop</a>
+            </li>
+            <li >
+                <a href="/"></a>
+            </li>
+        </ul>
+    </nav>
+   </div>
+</header>
